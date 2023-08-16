299e631b10c93d8da6320ab94a51f28a380f5ab7
harudee <harudee@naver.com>
2023-08-16 21:54:39 +0900

    feat. retrofit api, xml parcer, data class 구성

--- app/src/main/java/com/zzb/whichhospital/data/remote/RemoteDataSource.kt
+++ app/src/main/java/com/zzb/whichhospital/data/remote/RemoteDataSource.kt
@@ -1,4 +1,57 @@
 package com.zzb.whichhospital.data.remote
 
+import com.tickaroo.tikxml.annotation.Element
+import com.tickaroo.tikxml.annotation.PropertyElement
+import com.tickaroo.tikxml.annotation.Xml
+
+
+/**
+ * [data class]
+ * 진료과목 num, 병원명, 병원주소(위도, 경도), 병원 전화번호
+ * -> 병원명으로 네이버 앱에 검색해서 나오게하기
+ */
 class RemoteDataSource {
+    @Xml(name = "response")
+    data class Hospital(
+        @Element(name = "body")
+        val body : Body,
+        @Element(name = "header")
+        val header: Header
+    )
+
+    @Xml(name ="header")
+    data class Header(
+        @PropertyElement(name="resultCode")
+        val resultCode: Int,
+        @PropertyElement(name="resultMsg")
+        val resultMsg: String
+
+    )
+    @Xml(name = "body")
+    data class Body(
+        @Element(name ="items")
+        val items : Items
+
+    )
+
+    @Xml(name = "items")
+    data class Items(
+        @Element(name="item")
+        val item : List<Item>
+
+    )
+
+    @Xml(name = "item")
+    data class Item(
+        @PropertyElement(name="dutyName")
+        var dutyName : String,
+        @PropertyElement(name="dutyAddr")
+        var dutyAddr : String,
+        @PropertyElement(name="dgidIdName")
+        var dgidIdName : String,
+        @PropertyElement(name="dutyTel1")
+        var dutyTel1 : String
+
+    )
+
 }
--- app/src/main/java/com/zzb/whichhospital/di/RetrofitModule.kt
+++ app/src/main/java/com/zzb/whichhospital/di/RetrofitModule.kt
@@ -1,4 +1,97 @@
 package com.zzb.whichhospital.di
 
+import android.util.Log
+import com.tickaroo.tikxml.TikXml
+import com.tickaroo.tikxml.retrofit.TikXmlConverterFactory
+import com.zzb.whichhospital.data.remote.RemoteDataSource
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+/**
+ * 가져오기 getBlah()
+ *
+ * [data class]
+ * 진료과목 num, 병원명, 병원주소(위도, 경도), 병원 전화번호
+ * -> 병원명으로 네이버 앱에 검색해서 나오게하기
+ *
+ * [Retrofit]
+ * builder
+ * data
+ * interface
+ *
+ */
 class RetrofitModule {
+
+    /**
+     * Create Hospital API
+     */
+    companion object{
+
+        //https://apis.data.go.kr/B552657/HsptlAsembySearchService/  // : endPoint
+        val baseAPIURL = "https://apis.data.go.kr/B552657/HsptlAsembySearchService/getHsptlBassInfoInqire/"
+
+        var instance : Retrofit? = null
+        var xmlParcer = TikXml.Builder().exceptionOnUnreadXml(false).build()
+
+        fun getHospitalInstance() : Retrofit {
+            if(instance == null) {
+                instance = Retrofit.Builder()
+                    .baseUrl(baseAPIURL)
+                    .addConverterFactory(TikXmlConverterFactory.create(xmlParcer))
+                    .build()
+            }
+            return instance!!
+        }
+    }
+
+    /**
+     * Get Hospital Data
+     */
+    interface HospitalService{
+        companion object{
+            val serviceKey = "fu7gLt1kvMpPj77E5Veta7zhNds0R8JSKhu1LpI29orK4Uc8rqvbVd3Bytv8fL9rcy4xv6Effc20LAnpjKobFQ=="
+
+        }
+
+        @GET("getHospitalData")
+        fun getHospitalData(
+            @Query("serviceKey")
+            serviceKey : String = HospitalService.serviceKey
+
+        ) : Call<RemoteDataSource>
+
+
+    }
+
+    /**
+     * Request Hospital Data
+     */
+    fun requestHospitalData(){
+        val requestHospitalService = RetrofitModule.getHospitalInstance()
+            .create(RetrofitModule.HospitalService::class.java)
+        val hospitalCall = requestHospitalService.getHospitalData()
+
+        hospitalCall.enqueue(object : Callback<RemoteDataSource> {
+            override fun onResponse(
+                call: Call<RemoteDataSource>,
+                response: Response<RemoteDataSource>
+            ) {
+                if(response.isSuccessful && response.code() == 200){
+                    Log.d("=== Retrofit success ===", "onResponse: ")
+                }
+            }
+
+            override fun onFailure(call: Call<RemoteDataSource>, t: Throwable) {
+                Log.e("=== Retrofit fail ===", t.stackTraceToString())
+
+            }
+
+        })
+
+    }
+
 }
